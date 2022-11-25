f2e15978ba4fa335a0bce21d9f1dfb41dd4504f9
Christian Paul <koutoua12@gmail.com>
2022-11-25 17:05:18 +0800

    Fist Arangment All

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="Products" v-motion-slide-visible-once-bottom class="product"
+  <section id="Products"  class="product"
            style="min-height: fit-content;padding-top: 100px; padding-bottom: 50px;  height: fit-content; width: 100%;">
 
     <div class="container mb-5 h-100">
@@ -37,26 +37,25 @@
       </ul>
       <div  class="align-items-center overflow-hidden" style="min-height:fit-content; height: fit-content;">
 
-        <div style="height: fit-content; " class="container w-100">
-          <div class=" row flex-wrap d-none d-md-flex align-items-center justify-content-center listOfProducts"
-               style="height:100%;  width: fit-content;">
-               <div v-for="(item,index) in ImageList" :key="index" class="py-2 col-1 mix border-0" :class="items.at(index%4)"
-                   style="min-width: 200px; flex-shrink: 0;  height: 200px;">
-                <div class="border" style="">
-                  <!-- <el-image style="width: 100px; height: 150px" lazy="true" loading="eager" src="./images/1.jpg" fit="cover" /> -->
-                  <img src="" class="img" width="150" height="150"/>
-                  <h6>{{item}}</h6>
+        <div style="min-height: fit-content;"  class="container w-100 p-0 pb-3">
+          <div class="row flex-wrap d-none d-md-flex align-items-center g-5 justify-content-center listOfProducts"
+               style="min-height:fit-content;  width: fit-content;">
+               <div v-for="item,index in ImageList" :key="index" class="py-2 col-1 mix border-0" :class="items.at(index%4)" style="min-width: 200px; flex-shrink: 0;  height: 200px;">
+                <div class="" style="">
+                  <!-- <el-image style="width: 100%; height: 100%" :lazy="true" loading="eager" :src="item" fit="cover" /> -->
+                  <img  class="img shadow-sm" style="height: 180px; width:180px"   v-bind:src="item" v-bind:alt="item"/>
+                  <!-- <h6>{{item}}</h6> -->
                 </div>
               </div>
           </div>
           <el-scrollbar class="justify-content-evenly d-block d-md-none listOfProducts1"
                         style="height:fit-content;  width: 100%;">
             <div class="d-flex gap-4">
-              <div v-for="index in TAILLE" :key="index" class="py-2 col-1 mix border-0" :class="items.at(index%4)"
+              <div v-for="item,index in ImageList" :key="index" class="py-2 col-1 mix border-0" :class="items.at(index%4)"
                    style="min-width: 200px; flex-shrink: 0;  height: 200px;">
                 <div class="border">
-                  <img class="" height="150" src="./images/1.png"/>
-                  <h6>link {{this.imgLink(index)}}</h6>
+                  <img class="" height="150" :src="item"/>
+                  <!-- <h6>link {{this.imgLink(index)}}</h6> -->
                 </div>
               </div>
             </div>
@@ -82,6 +81,9 @@ import 'jquery/dist/jquery.min.js'
 import mixitup from 'mixitup';
 
 const links = document.getElementsByClassName("nav-link item")
+const items = document.getElementsByClassName("img")
+
+
 export default {
   components: {},
   data() {
@@ -95,21 +97,21 @@ export default {
         "re"
       ],
       ImageList:{
-        0:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-        1:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-        2:"./images/2.jpg",
-        3:"./images/3.jpg",
-        4:"./images/4.jpg",
-        5:"./images/5.jpg",
-        6:"./images/6.jpg",
-        7:"./images/7.jpg",
-        8:"./images/8.jpg",
-        9:"./images/9.jpg",
-        10:"./images/10.jpg",
-        11:"./images/11.jpg",
-        12:"./images/12.jpg",
-        13:"./images/13.jpg",
-        14:"./images/14.jpg",
+        0:"./images/1.jpg",
+        1:"./images/2.jpg",
+        2:'./images/3.jpg',
+        3:"./images/4.jpg",
+        4:"./images/5.jpg",
+        5:"./images/6.jpg",
+        6:"./images/7.jpg",
+        7:"./images/8.jpg",
+        8:"./images/9.jpg",
+        9:"./images/10.jpg",
+        10:"./images/11.jpg",
+        11:"./images/12.jpg",
+        12:"./images/13.jpg",
+        13:"./images/14.jpg",
+        // 14:"./images/15.jpg",
       }
     }
   },
@@ -139,26 +141,29 @@ export default {
         animateResizeContainer: true
       }
     })
-
     mixer1.filter('all')
 
+        // console.log(items);
+    var index=0;
+    for(index ; index<items.length;index++){
+      const img = items[index]
+      // img.setAttribute("src", this.ImageList[index])
+      
+      // img.attribute=this.ImageList[index]
+      // console.log(img);
+    }
   },
   created(){
 
-    const items = document.getElementsByClassName("img")
-console.log(items);
-var index=0;
-    for(index ; index<items.length;index++){
-      const img = items[index]
-      console.log(index);
-    }
+
+
   },
   methods: {
     imgLink(index){
       
     },
     getActive(option) {
-      console.log(links);
+      // console.log(links);
       for (let optionKey = 0; optionKey < links.length; optionKey++) {
         if (option === optionKey) {
           links[optionKey].classList.add("active")
